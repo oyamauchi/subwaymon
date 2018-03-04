3031ea17984b24e7cbe5f52ffe8eae6480112178
Owen Yamauchi <owen.yamauchi@gmail.com>
2018-03-04 16:41:12 -0500

    Fix some warnings

--- SubwayMon/SwiftTrainView.swift
+++ SubwayMon/SwiftTrainView.swift
@@ -19,7 +19,7 @@ class SwiftTrainView: NSView {
   private func truncate(text: String, withAttributes attributes: Dictionary<String, Any>, toWidth width: CGFloat) -> String {
     var attempt = text
 
-    while (attempt.size(withAttributes: attributes).width > width && attempt.characters.count > 1) {
+    while (attempt.size(withAttributes: attributes).width > width && attempt.count > 1) {
       let range = attempt.index(attempt.endIndex, offsetBy: -2)..<attempt.endIndex
       attempt = attempt.replacingCharacters(in: range, with: "\u{2026}")
     }
@@ -51,9 +51,9 @@ class SwiftTrainView: NSView {
 
     // These values will be used for all the text
     let fontSize = 0.84 * self.bounds.size.height
-    let giantWhiteText = [
+    let giantWhiteText: Dictionary<String, Any> = [
       NSForegroundColorAttributeName: NSColor.white,
-      NSFontAttributeName: NSFont(name: "Helvetica Bold", size: fontSize)
+      NSFontAttributeName: NSFont(name: "Helvetica Bold", size: fontSize) as Any
     ]
 
     // Draw the route bullet. It should be a square that fills the height of this view,
