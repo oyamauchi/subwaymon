beaaab183ad3d0da66bfe53666a620f3758e7fe9
Owen Yamauchi <owen.yamauchi@gmail.com>
2021-04-28 14:52:10 -0400

    Save and restore selections in user defaults
    Fixes #2

--- SubwayApp/AppDelegate.swift
+++ SubwayApp/AppDelegate.swift
@@ -10,30 +10,51 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+  private let providerDefaultsKey = "SelectedProvider"
+  private let routeDefaultsKey = "SelectedRoute"
+  private let stopDefaultsKey = "SelectedStop"
+
   @IBOutlet var window: NSWindow!
   @IBOutlet var subway: SubwayMonView!
   @IBOutlet var providerMenu: NSPopUpButton!
   @IBOutlet var routeMenu: NSPopUpButton!
   @IBOutlet var stopMenu: NSPopUpButton!
 
-  var feedInfo: FeedInfo!
+  private var feedInfo: FeedInfo!
+  private let defaults: UserDefaults = UserDefaults.standard
+
+  private func selectItem(inMenu menu: NSPopUpButton, withDefaultsKey key: String) {
+    if let savedTitle = defaults.string(forKey: key) {
+      menu.selectItem(withTitle: savedTitle)
+    }
+    if menu.selectedTag() < 0 {
+      defaults.removeObject(forKey: key)
+      menu.selectItem(at: 0)
+    }
+  }
 
   @IBAction func providerMenuSelected(_ sender: NSPopUpButton) {
+    defaults.set(sender.titleOfSelectedItem, forKey: providerDefaultsKey)
+
     feedInfo = FeedInfo.feedInfo(forTag: sender.selectedTag())
     routeMenu.menu = feedInfo.routeMenu
     routeMenu.isEnabled = true
-    routeMenu.selectItem(at: 0)
+    selectItem(inMenu: routeMenu, withDefaultsKey: routeDefaultsKey)
     routeMenuSelected(routeMenu)
   }
 
   @IBAction func routeMenuSelected(_ sender: NSPopUpButton) {
+    defaults.set(sender.titleOfSelectedItem, forKey: routeDefaultsKey)
+
     stopMenu.menu = feedInfo.stopMenu(forRouteTag: sender.selectedTag())
     stopMenu.isEnabled = true
-    stopMenu.selectItem(at: 0)
+    selectItem(inMenu: stopMenu, withDefaultsKey: stopDefaultsKey)
     stopMenuSelected(stopMenu)
   }
 
   @IBAction func stopMenuSelected(_ sender: NSPopUpButton) {
+    defaults.set(sender.titleOfSelectedItem, forKey: stopDefaultsKey)
+
     let stopIds = feedInfo.stopIdsFor(stopTag: sender.selectedTag())
     subway.setStopIds(stopIds: stopIds, feedInfo: feedInfo)
     subway.needsDisplay = true
@@ -46,7 +67,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
   func applicationDidFinishLaunching(_: Notification) {
     providerMenu.menu = FeedInfo.providerMenu
-    providerMenu.selectItem(at: 0)
+    selectItem(inMenu: providerMenu, withDefaultsKey: providerDefaultsKey)
     providerMenuSelected(providerMenu)
 
     Timer.scheduledTimer(
